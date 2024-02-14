e3162603137c86743d0c2c71aa151017c1551b17
KAWINRAJU P <128171446+kawincodes@users.noreply.github.com>
2024-02-14 23:13:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,18 +50,18 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'KRS V4'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
-global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
+global.packname = 'KRS' 
+global.author = '@KAWINRAJY' 
+global.menuvid = 'https://files.krs.ninja/k.png'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/kawinraju\n' 
+global.dygp = 'https://youtube.com/@kawinraju'
+global.fgsc = 'https://youtube.com/@kawinraju' 
+global.fgyt = 'https://youtube.com/@kawinraju'
+global.fgpyp = 'https://youtube.com/@kawinraju'
+global.fglog = 'https://files.krs.ninja/k.png' 
+global.thumb = 'https://files.krs.ninja/k.png'
 
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
